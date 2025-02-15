f4266f31ab943be30b6806e7366b6447fb67f902
Phan Cao <caopv@magenest.com>
2025-02-15 16:39:55 +0700

    btq-13: manage group customer

---
+++ Modules/GroupCustomer/Repositories/GroupCustomerRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Modules\GroupCustomer\Repositories;
+
+use App\Repositories\BaseRepository;
+use Modules\GroupCustomer\Models\GroupCustomer;
+
+
+class GroupCustomerRepository extends BaseRepository implements GroupCustomerRepositoryInterface
+{
+    public function getModel() : string
+    {
+        return GroupCustomer::class;
+    }
+}
---
+++ Modules/GroupCustomer/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('groupcustomer::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>Module: {!! config('groupcustomer.name') !!}</p>
+@endsection
