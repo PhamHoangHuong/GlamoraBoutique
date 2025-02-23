e98e67a9a4d79af298bd61b6ce39a3627bc38122
Hoang Huong | apikey:d0851193d83d8c0dc0833e050f2c432f59dd585145eb42c2440363789246f383 <huong.ph@fsofts.com>
2025-02-23 13:44:27 +0700

    save lỗi page

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
@@ -15,6 +14,7 @@ export default defineConfig(({ command, mode }) => {
             laravel({
                 input: ['resources/js/app.tsx', 'resources/scss/app.scss'],
                 refresh: true,
+                buildDirectory: 'build',
             }),
             react(),
         ],
@@ -30,69 +30,35 @@ export default defineConfig(({ command, mode }) => {
                 host: HMR_HOST,
                 protocol: 'ws',
             },
-            // Allow connections from any origin
             cors: true,
         },
         build: {
             manifest: true,
             outDir: 'public/build',
             assetsDir: '',
+            minify: true,
+            sourcemap: false,
             rollupOptions: {
                 output: {
                     manualChunks: undefined,
-                    chunkFileNames: 'js/[name]-[hash].js',
-                    entryFileNames: 'js/[name]-[hash].js',
-                    assetFileNames: '[ext]/[name]-[hash].[ext]'
+                    chunkFileNames: 'js/[name].js',
+                    entryFileNames: 'js/[name].js',
+                    assetFileNames: (assetInfo) => {
+                        if (assetInfo.name.endsWith('.css')) {
+                            return 'css/[name][extname]';
+                        }
+                        if (assetInfo.name.endsWith('.js')) {
+                            return 'js/[name][extname]';
+                        }
+                        return 'assets/[name][extname]';
+                    },
                 }
             }
-        }
+        },
+        optimizeDeps: {
+            esbuildOptions: {
+                target: 'es2015',
+            },
+        },
     };
-=======
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-	plugins: [
-		laravel({
-			input: ["resources/js/app.tsx", "resources/scss/app.scss"],
-			refresh: true,
-			buildDirectory: "build",
-		}),
-		react(),
-	],
-	resolve: {
-		alias: {
-			"@": "/resources/js",
-		},
-	},
-	build: {
-		manifest: true,
-		outDir: "public/build",
-		assetsDir: "",
-		minify: true,
-		sourcemap: false,
-		rollupOptions: {
-			output: {
-				manualChunks: undefined,
-				chunkFileNames: "js/[name].js",
-				entryFileNames: "js/[name].js",
-				assetFileNames: (assetInfo) => {
-					if (assetInfo.name.endsWith(".css")) {
-						return "css/[name][extname]";
-					}
-					if (assetInfo.name.endsWith(".js")) {
-						return "js/[name][extname]";
-					}
-					return "assets/[name][extname]";
-				},
-			},
-		},
-	},
-	optimizeDeps: {
-		esbuildOptions: {
-			target: "es2015",
-		},
-	},
->>>>>>> Stashed changes
 });
